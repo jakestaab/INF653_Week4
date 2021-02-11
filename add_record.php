aa34e041c69cdd0b855e16b75936e553f8d8e2bc
Jake Staab <jjstaab@mail.fhsu.edu>
2021-02-11 08:15:13 -0600

    finally got table to work correctly

--- add_record.php
+++ add_record.php
@@ -1,7 +1,6 @@
 <?php
 require('database.php');
 
-//$itemnum = filter_input(INPUT_POST, 'itemnum', FILTER_VALIDATE_INT);
 $title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING);
 $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_STRING);
 
@@ -11,7 +10,6 @@ if ($title) {
                 VALUES
                 (:title, :description)';
     $statement = $db->prepare($query);
-    //$statement->bindValue(':itemnum', $itemnum);
     $statement->bindValue(':title', $title);
     $statement->bindValue(':description', $description);
     $statement->execute();
--- index.php
+++ index.php
@@ -3,9 +3,6 @@
     $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
     $description = filter_input(INPUT_GET, "description", FILTER_SANITIZE_STRING);
 
-?>
-
-<?php
         $query = 'SELECT * FROM todoitems
                         ORDER BY ItemNum ASC';
         $statement = $db->prepare($query);
@@ -21,40 +18,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>To Do LIst</title>
+    <title>ToDo LIst</title>
     <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
     <main>
         <header>
-            <h1>To Do List</h1>
+            <h1>ToDo List</h1>
         </header>
 
         <?php if(!empty($results)) { ?>
-            <secton>
-                <h2>Items</h2>
-                <table>
+            <section>
                     <?php foreach($results as $result) {
                         $itemnum = $result['ItemNum'];
                         $title = $result['Title'];
                         $description = $result['Description'];
                     ?>
+                <table>
                     <tr>
-                        <td><?php echo $title; ?></td>
-                        <td><?php echo $description; ?></td>
+                        <td><span class="title"><?php echo $title; ?></span>
+                            <div><?php echo $description; ?></div></td>
                         <td><form action="delete_record.php" method="POST">
                             <input type="hidden" name="itemnum" value="<?php echo $itemnum ?>">
-                            <button class="delete">Delete</button>
+                            <button class="delete">&#10006</button>
                         </form></td>
                     </tr>
                 </table>
             <?php } ?>
             </section>
         <?php } else { ?>
-            <p>Sorry, no results.</p>
+            <p>There are no items on the ToDo List.</p>
         <?php } ?>
-        <section>
 
+        <section>
             <h2>ADD ITEM</h2>
             <form action="add_record.php" method="POST">
                 <input type="hidden" name="itemnum" value="<?php echo $itemnum ?>">
@@ -62,9 +58,10 @@
                 <input type="text" id="title" name="title" required>
                 <label for="description">Description:</label>
                 <input type="text" id="description" name="description" required>
-                <button>Submit</button>
+                <button>Add Item</button>
             </form>
         </section>
+
     </main>
 </body>
 </html>
